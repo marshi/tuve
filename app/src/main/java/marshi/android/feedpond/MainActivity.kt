d7213ce1765ad4dcd10b47f368a99b2f62c28131
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2018-09-10 02:40:02 +0900

    databinding.

--- app/src/main/java/marshi/android/feedpond/MainActivity.kt
+++ app/src/main/java/marshi/android/feedpond/MainActivity.kt
@@ -2,22 +2,26 @@ package marshi.android.feedpond
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import marshi.android.feedpond.databinding.ActivityMainBinding
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector  {
 
     @Inject lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onSupportNavigateUp(): Boolean {
