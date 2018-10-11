a3692ba619ab4316c9623954d4c811513f2b7149
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2018-10-12 00:35:18 +0900

    room

---
+++ repository/src/main/java/marshi/android/tuve/repository/database/AppDatabase.kt
@@ -0,0 +1,9 @@
+package marshi.android.tuve.repository.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+}
---
+++ repository/src/main/java/marshi/android/tuve/repository/database/entity/Follow.kt
@@ -0,0 +1,9 @@
+package marshi.android.tuve.repository.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Follow(
+        @PrimaryKey private val id: Int
+)
